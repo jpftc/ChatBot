75b9eb49cb7f533ea12758bb99f84c731ec36c6e
João Teixeira <jpteixeira1308@gmail.com>
2021-07-13 13:51:57 -0300

    Primeiro commit

---
+++ src/menu/menu0.js
@@ -0,0 +1,20 @@
+const cardapio0 = {
+  1: {
+    description: 'Morfador',
+    price: '350.00',
+  },
+  2: {
+    description: 'Arma Laser',
+    price: '600.95',
+  },
+  3: {
+    description: 'Todos os episódios em VHS',
+    price: '4000.00',
+  },
+  4: {
+    description: 'Megazord',
+    price: '999.999.999',
+  },
+}
+
+exports.menu0 = cardapio0
